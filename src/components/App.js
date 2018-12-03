9fa3a17bb0406759b5be3cb71a535bb692a55a4f
fieldr <fieldr92@gmail.com>
2018-12-03 11:30:38 +0000

    Add video play functionality via iFrame - update styling also

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import youtube from '../api/youtube';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
-import youtube from '../api/youtube';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
+  }
+
+  componentDidMount() {
+    this.onTermSubmit('');
   }
 
   onTermSubmit = async term => {
@@ -14,16 +20,34 @@ class App extends React.Component {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    const idRandomiser = Math.floor(Math.random() * response.data.items.length);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[idRandomiser]
+    });
+  }
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar
-          onSubmit={this.onTermSubmit}
-        />
-        <VideoList videos={ this.state.videos } />
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={ this.state.videos }
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return (
+      <div className="ui segment">
+        <div className="ui active inverted dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
+        <p></p>
+      </div>
+    )
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} title="Video Player" />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
